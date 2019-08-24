92d5b7a21f3b0308a4828b918128ffe61a6d8243
Tyebile <songbo@htah.com.cn>
2019-08-24 17:37:41 +0800

    twosum & judgeCircle

--- src/main/java/com/tyebile/leetcode/one/TwoSum.java
+++ src/main/java/com/tyebile/leetcode/one/TwoSum.java
@@ -1,8 +1,37 @@
 package com.tyebile.leetcode.one;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @author Tyebile
  * @date 2019/8/24
  */
 public class TwoSum {
+
+    public static void main(String[] args) {
+        int target = 6;
+        int[] nums = new int[]{3,2,4};
+
+        nums = twoSum(nums, target);
+        Arrays.stream(nums)
+                .forEach(System.out::println);
+    }
+
+    private static int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>(4);
+
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i],i);
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            int left = target - nums[i];
+            if(map.containsKey(left) && map.get(left) != i){
+                return new int[]{i,map.get(left)};
+            }
+        }
+        return new int[]{};
+    }
 }
--- src/main/java/com/tyebile/leetcode/six/JudgeCircle.java
+++ src/main/java/com/tyebile/leetcode/six/JudgeCircle.java
@@ -5,4 +5,31 @@ package com.tyebile.leetcode.six;
  * @date 2019/8/24
  */
 public class JudgeCircle {
+
+    public static void main(String[] args) {
+        String moves = "UDLRLR";
+        System.out.println(judgeCircle(moves));
+    }
+
+    private static boolean judgeCircle(String moves) {
+        int x = 0;
+        int y = 0;
+        for (int i = 0; i < moves.length(); i++) {
+            switch (moves.charAt(i)){
+                case 'U':
+                    x++;
+                    break;
+                case 'D':
+                    x--;
+                    break;
+                case 'L':
+                    y++;
+                    break;
+                case 'R':
+                    y--;
+                    break;
+            }
+        }
+        return x==0 && y==0;
+    }
 }
